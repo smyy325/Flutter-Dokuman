0b3afa47d0e08e46133fa5868028e50f8ea8072a
smyy325 <sumeyyea325@gmail.com>
2024-11-25 12:57:46 +0300

    Day3 commit1

--- lib/Day3/composition.dart
+++ lib/Day3/composition.dart
@@ -31,7 +31,7 @@ void main(){
   var k1 = Kategoriler(kategori_id: 1, kategori_ad: "Dram");
   var k2 = Kategoriler(kategori_id: 2, kategori_ad: "Bilim Kurgu");
 
-  var y1 = Yonetmenler(yonetmen_id: 1, yonetmen_ad: "Quention Tarantino");
+  var y1 = Yonetmenler(yonetmen_id: 1, yonetmen_ad: "Quentin Tarantino");
   var y2 = Yonetmenler(yonetmen_id: 2, yonetmen_ad: "Christopher Nolan");
 
   var f1 = Filmler(film_id: 1, film_ad: "Django", film_yil: 2013, kategori: k1, yonetmen: y1);
--- lib/Day3/kalitim.dart
+++ lib/Day3/kalitim.dart
@@ -1,20 +1,41 @@
 class Ev{
-  int pencereSayis;
-  Ev({required this.pencereSayis});
+  int pencereSayisi;
+  Ev({required this.pencereSayisi});
 }
 
 class Saray extends Ev{
   int kuleSayisi;
 
-  Saray({required this.kuleSayisi, required int pencereSayis}) : super (pencereSayis: 100);
+  Saray({required this.kuleSayisi, required int pencereSayisi}) : super (pencereSayisi: pencereSayisi);
 }
 
 class Villa extends Ev{
   bool garajVarMi;
 
-  Villa({required this.garajVarMi}) : super (pencereSayis: 100);
+  Villa({required this.garajVarMi, required pencereSayisi}) : super (pencereSayisi: pencereSayisi);
 }
 
 void main(){
+  var topkapiSarayi = Saray(kuleSayisi: 10, pencereSayisi: 300);
+  print(topkapiSarayi.kuleSayisi);
+  print(topkapiSarayi.pencereSayisi);
 
+  var bogazVilla = Villa(garajVarMi: true, pencereSayisi: 50);
+  print(bogazVilla.garajVarMi);
+  print(bogazVilla.pencereSayisi);
+/*
+  // Tip Kontrolü
+  if(topkapiSarayi is Saray){
+    print("Saraydır");
+  }else{
+    print("Saray değildir");
+  }
+
+  //Downcasting
+  var ev = Ev(pencereSayisi: 10);
+  var saray = ev as Saray;
+
+  //Upcasting
+  var s = Saray(kuleSayisi: 3, pencereSayisi: 100);
+  Ev e = s;*/
 }
